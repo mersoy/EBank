ee36f640448e911d68eca61cf3846b31cac142c5
Muge Ersoy <muge.ersoy@schibsted.com>
2018-02-04 13:24:06 +0000

    removing unused files

--- E-Bank/AccountList/AccountViewFactory.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2018 Muge Ersoy. All rights reserved.
-
-import UIKit
-
-class AccountViewFactory {
-    func makeAccountList() -> UIViewController {
-        return UIStoryboard(name: "Main").instantiateViewController(withIdentifier: "AccountListNController")
-    }
-}
